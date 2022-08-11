ad1349b622c709efa9366594ec0684e883c6b907
wmm4249 <wmars503@gmail.com>
2022-08-11 09:22:11 -0700

    Add a rough draft of the question roster

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   $("#formOne").submit(function(e) {
     e.preventDefault();
     const aspect = $("#aspect").val();
+    const q2 = $("#q2").val();
+    const q3 = $("#q3").val();
+    const q4 = $("#q4").val();
+    const q5 = $("#q5").val();
 
     $("#output").html(result);
 });
