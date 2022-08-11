b618a563f2fb6a91905042b49394a7f5882a5a35
wmm4249 <wmars503@gmail.com>
2022-08-11 11:42:01 -0700

    Debug the submit form

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
   $("#formOne").submit(function(e) {
     e.preventDefault();
-    const aspect = $("#aspect").val();
-    const q2 = $("#q2").val();
-    const q3 = $("#q3").val();
-    const q4 = $("#q4").val();
-    const q5 = $("#q5").val();
+    console.log("submitted");
+    const aspect = parseInt($("#aspect").val());
+    const q2 = parseInt($("#q2").val());
+    const q3 = parseInt($("#q3").val());
+    const q4 = parseInt($("#q4").val());
+    const q5 = parseInt($("#q5").val());
+    let result;
+    let resultScore = aspect + q2 + q3 + q4 + q5;
+
+    console.log("aspect: ", aspect, typeof aspect);
+
+    if (resultScore = 5) {
+      result = "You should learn Javascript.";
+    } else if (resultScore >= 6 || resultScore <= 8) {
+      result = "You should learn Python.";
+    } else if (resultScore >= 9 || resultScore <= 10) {
+      result = "You should learn C#.";
+    } else if (resultScore >= 11 || resultScore <= 20) {
+      result = "You should learn Ruby.";
+    }    
 
     $("#output").html(result);
-});
+  });
 });
