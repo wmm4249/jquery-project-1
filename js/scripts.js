9d41cae6285e1edb9b60450868d46924321c71ab
wmm4249 <wmars503@gmail.com>
2023-01-04 09:06:57 -0800

    Change first section of back end logic to Vanilla JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(e) {
+document.ready(function() {
+    getElementById("formOne").submit(function(e) {
     e.preventDefault();
-    const aspect = parseInt($("#aspect").val());
-    const q2 = parseInt($("#q2").val());
-    const q3 = parseInt($("#q3").val());
-    const q4 = parseInt($("#q4").val());
-    const q5 = parseInt($("#q5").val());
+    const aspect = parseInt(document.getElementById("aspect").value);
+    const q2 = parseInt(document.getElementById("q2").value);
+    const q3 = parseInt(document.getElementById("q3").value);
+    const q4 = parseInt(document.getElementById("q4").value);
+    const q5 = parseInt(document.getElementById("q5").value);
     let result;
     let resultScore = aspect + q2 + q3 + q4 + q5;
 
@@ -31,3 +31,28 @@ $(document).ready(function() {
   $(".result").hide();
   });
 });
+
+
+
+//     if (resultScore == 5 || resultScore <= 8) {
+//       result = "You should learn Javascript.";
+//     } else if (resultScore >= 9 && resultScore <= 12) {
+//       result = "You should learn Python.";
+//     } else if (resultScore >= 13 && resultScore <= 16) {
+//       result = "You should learn C#.";
+//     } else if (resultScore >= 17 && resultScore <= 20) {
+//       result = "You should learn Ruby.";
+//     }    
+
+//     document.getElementByClassName("result").style.display = "";
+//     document.getElementById("survey").style.display = "none";
+
+//     document.getElementById("output").innerHTML = result;
+//   });
+//   document.getElementById("reload-page").click(function() {
+
+//   document.getElementById("survey").style.display = "";
+
+//   document.getElementByClassName("result").style.display = "none";
+//   });
+// });
