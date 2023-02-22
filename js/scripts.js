63b992e1552295bb0b5847c368d5f487d0d79d29
wmm4249 <wmars503@gmail.com>
2023-02-22 11:08:13 -0800

    Make the webpage function closer to its proper manner

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,43 @@
-window.onload = function () {
+function handleSubmit(event) {
+  event.preventDefault();
   const form = document.getElementById("formOne");
-  function handleSubmit(event) {
-    event.preventDefault();
-    const selection = document.getElementById("survey").value;
-    const aspect = parseInt(document.getElementById("aspect").value);
-    const q2 = parseInt(document.getElementById("q2").value);
-    const q3 = parseInt(document.getElementById("q3").value);
-    const q4 = parseInt(document.getElementById("q4").value);
-    const q5 = parseInt(document.getElementById("q5").value);
-    let result;
-    let resultScore = aspect + q2 + q3 + q4 + q5;
+  const selection = document.getElementById("survey").value;
+  const aspect = parseInt(document.getElementById("aspect").value);
+  const q2 = parseInt(document.getElementById("q2").value);
+  const q3 = parseInt(document.getElementById("q3").value);
+  const q4 = parseInt(document.getElementById("q4").value);
+  const q5 = parseInt(document.getElementById("q5").value);
+  let result;
+  let resultScore = aspect + q2 + q3 + q4 + q5;
 
-    if (resultScore == 5 || resultScore <= 8) {
-      result = "You should learn Javascript.";
-    } else if (resultScore >= 9 && resultScore <= 12) {
-      result = "You should learn Python.";
-    } else if (resultScore >= 13 && resultScore <= 16) {
-      result = "You should learn C#.";
-    } else if (resultScore >= 17 && resultScore <= 20) {
-      result = "You should learn Ruby.";
-    }
-    console.log("result: ", result)
-
-  
-  window.addEventListener("load", function() {
-    document.getElementById("formOne").addEventListener("submit", handleSubmit);
-    
-
-  document.getElementByClassName("result").style.display = "initial";
+  if (resultScore == 5 || resultScore <= 8) {
+    result = "You should learn Javascript.";
+  } else if (resultScore >= 9 && resultScore <= 12) {
+    result = "You should learn Python.";
+  } else if (resultScore >= 13 && resultScore <= 16) {
+    result = "You should learn C#.";
+  } else if (resultScore >= 17 && resultScore <= 20) {
+    result = "You should learn Ruby.";
+  }
 
   document.getElementById("survey").style.display = "none";
+  document.getElementById("resultDiv").style.display = "block";
 
   document.getElementById("output").innerHTML = result;
+}
+  
+window.addEventListener("load", function() {
+  document.getElementById("formOne").addEventListener("submit", handleSubmit);
+  document.getElementById("reload-page").addEventListener("click", reloadPage);
+});
 
-  })};
-  document.getElementById("reload-page").click = function () {
+function reloadPage() { 
 
-    document.getElementById("survey").style.display = "";
+  document.getElementById("survey").style.display = "";
 
-    document.getElementByClassName("result").style.display = "none";
-  };
+  document.getElementById("resultDiv").style.display = "none";
 };
+// };
 
 // $(document).ready(function() {
 //   $("#formOne").submit(function(e) {
